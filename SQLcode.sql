d7e53b1cf22dd32b98a1bb7c67805779fad07fa8
Kang Jeong Hyeon <83824814+Puzzle928@users.noreply.github.com>
2021-06-21 17:04:26 +0900

    5:04

--- SQLcode.sql
+++ SQLcode.sql
@@ -1,6 +1,6 @@
 create table Company(
     COM_NO number(3) primary key,
-    NAME varchar(30) constraint Company_NAME_nn not null,
+    CNAME varchar(30) constraint Company_NAME_nn not null,
     INDUSTRY varchar2(30) constraint Company_INDUSTRY_nn not null,
     TECH_STACK varchar2(30) constraint Company_TECH_STACK_nn not null,
     SALARY number(4),
@@ -13,30 +13,28 @@ create table Company(
 
 create table Board(
     COM_NO number(3) constraint  Board_COM_NO_fk  references Company(COM_NO),
-    ID number(3) constraint Company_F_IMPRESSION_nn not null,
-    COMPANY number(3 constraint Company_F_IMPRESSION_nn not null    
+    ID number(3) constraint Board_ID_nn not null,
+    CNAME number(3) constraint Board_CNAME_fk references Company(CNAME),
+    SCORE number(1) constraint Board_SCORE_nn not null,
+    REVIEW varchar(50)
+
 );
 
 insert all
-     into Company values(01, 'BankSalad', 'finance', 'MachineLearning', 3392, 'small', 'newcomer', 62.84 )
-     into Company values(02, 'Shinhan Bank','Analyze Data','knowingbro', 5500, 'big', 9.84)
-     into Company values(03, 'Kakao Bank', 'finance', 'MachineLearning', 3684, 'big', 13.54)
-     into Company values(04, 'Tesla', 'automobile industry', 'Autonomous driving', 14520, 'big')
-     into Company values(05, 'Hyundai Motors', 'automobile industry', 'Autonomous driving', 6500, 'big')
-     into Company values(06, 'Kia Motors', 'automobile industry', 'MachineLearning', 4344, 'big')
-     into Company values(07, 'Inbody', 'Medical', 'Image', 4000, 'medium')
-     into Company values(08, 'Cizen', 'Medical', 'Genetic Algorithm', 4000, 'medium')
-     into Company values(09, 'Vuno', 'Medical', 'Image', 4000, 'medium')
-     into Company values(10, 'ADT Caps', 'Security', 'Cloud', 3016)
-     into Company values(11, 'Ahn Lab', 'Security', 'Anti-Virus', 4836)
-     into Company values(12, 'Secui', 'Security', 'Cloud Security', 5416)
-     into Company values(13, 'Naver', 'IT Service', 'Platform', 10247)
-     into Company values(14, 'KaKao', 'IT Service', 'Applications', 13200)
-     into Company values(15, 'Google', 'IT Service', 'Voice recognition', 16000)
+     into Company values(01, 'BankSalad', 'finance', 'MachineLearning', 3392, 'small', 'newcomer')
+     into Company values(02, 'Shinhan Bank','finance','Analyze Data', 3862, 'big', 'newcomer')
+     into Company values(03, 'Kakao Bank', 'finance', 'MachineLearning', 3684, 'big', 'experience')
+     into Company values(04, 'Tesla', 'automobile industry', 'Autonomous driving', 14520, 'big', 'experience')
+     into Company values(05, 'Hyundai Motors', 'automobile industry', 'Autonomous driving', 4370, 'big', 'newcomer')
+     into Company values(06, 'Kia Motors', 'automobile industry', 'MachineLearning', 4344, 'big', 'experience')
+     into Company values(07, 'Inbody', 'Medical', 'Image', 4246, 'medium', 'newcomer')
+     into Company values(08, 'Cizen', 'Medical', 'Genetic Algorithm', 3897, 'medium', 'experience')
+     into Company values(09, 'Vuno', 'Medical', 'Image', 3682, 'medium', 'experience')
+     into Company values(10, 'ADT Caps', 'Security', 'Cloud Security', 3016, 'big', 'experience')
+     into Company values(11, 'Ahn Lab', 'Security', 'Anti-Virus', 3000, 'medium', 'experience')
+     into Company values(12, 'Secui', 'Security', 'Cloud Security', 5416, 'big', 'newcomer')
+     into Company values(13, 'Naver', 'IT Service', 'Platform', 4500, 'big', 'newcomer')
+     into Company values(14, 'KaKao', 'IT Service', 'Applications', 4200, 'big', 'newcomer')
+     into Company values(15, 'Google Korea', 'IT Service', 'Voice recognition', 4055 , 'big', 'newcomer')
 select * from dual;
 commit;
-
--- insert into Impression values(01, 02, 03)
--- insert into Impression values(02, 01, 04)
--- insert into Impression values(03, 04, 01)
--- insert into Impression values(04, 03, 02)

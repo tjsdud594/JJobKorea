5cd975e92b2448f73682de2f7a9309f58f4bac6d
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-06-23 00:55:55 +0900

    sal_grade 수정

---
+++ SQLcode.sql
@@ -0,0 +1,62 @@
+create table Company(
+    COM_NO number(3) primary key,
+    CNAME varchar2(30) constraint Company_CNAME_nn not null unique,
+    INDUSTRY varchar2(30) constraint Company_INDUSTRY_nn not null,
+    TECH_STACK varchar2(30) constraint Company_TECH_STACK_nn not null,
+    SAL_GRADE number(3) constraint Company_SAL_GRADE_nn not null, 
+    SCALE varchar2(10) constraint Company_SCALE_nn not null,
+    HIRECAREER varchar2(10) constraint Company_HIRECARREER_nn not null
+);
+
+create table Board(
+    ID number(3) constraint Board_ID_nn not null,
+    CNAME varchar2(30),
+    SCORE number(1) constraint Board_SCORE_nn not null,
+    REVIEW varchar2(50),
+    constraint FK_Board_CNAME foreign key (CNAME) references Company (CNAME)
+);
+
+create table Salary(
+    CNAME varchar2(30),
+    SALARY number(5) constraint Salary_SALARY_nn not null,
+    SAL_GRADE number(3) constraint Salary_SAL_GRADE_nn not null,
+    SAL_RANGE varchar2(50),
+    constraint FK_Salary_CNAME foreign key (CNAME) references Company (CNAME)
+);
+
+insert all
+     into Company values(01, 'BankSalad', 'Finance', 'MachineLearning', 'three', 'Small', 'Newcomer')
+     into Company values(02, 'Shinhan Bank','Finance','Analyze_Data', 'three', 'Big', 'Newcomer')
+     into Company values(03, 'Kakao Bank', 'Finance', 'MachineLearning', 'three', 'Big', 'Experience')
+     into Company values(04, 'Tesla', 'Automobile', 'Autonomous_Driving', 'more', 'Big', 'Experience')
+     into Company values(05, 'Hyundai Motors', 'Automobile', 'Autonomous_Driving', 'four', 'Big', 'Newcomer')
+     into Company values(06, 'Kia Motors', 'Automobile', 'MachineLearning', 'four', 'Big', 'Experience')
+     into Company values(07, 'Inbody', 'Medical', 'Image', 'four', 'Medium', 'Newcomer')
+     into Company values(08, 'Cizen', 'Medical', 'Genetic_Algorithm', 'three', 'Medium', 'Experience')
+     into Company values(09, 'Vuno', 'Medical', 'Image', 'three', 'Medium', 'Experience')
+     into Company values(10, 'ADT Caps', 'Security', 'Cloud_Security', 'three', 'Big', 'Experience')
+     into Company values(11, 'Ahn Lab', 'Security', 'Anti_Virus', 'three', 'Medium', 'Experience')
+     into Company values(12, 'Secui', 'Security', 'Cloud_Security', 'three', 'Big', 'Newcomer')
+     into Company values(13, 'Naver', 'IT_Service', 'Platform', 'four', 'Big', 'Newcomer')
+     into Company values(14, 'KaKao', 'IT_Service', 'Applications', 'four', 'Big', 'Newcomer')
+     into Company values(15, 'Google Korea', 'IT_Service', 'Platform', 'four', 'Big', 'Newcomer')
+select * from dual;
+
+insert all
+    into Salary values('BankSalad', 3392, 'three', '3000~4000')
+    into Salary values('Shinhan Bank', 3862, 'three', '3000~4000')
+    into Salary values('Kakao Bank', 3684, 'three', '3000~4000')
+    into Salary values('Tesla', 14520, 'more', '1억 이상')
+    into Salary values('Hyundai Motors', 4370, 'four', '4000~5000')
+    into Salary values('Kia Motors', 4344, 'four', '4000~5000')
+    into Salary values('Inbody', 4246, 'four', '4000~5000')
+    into Salary values('Cizen', 3897, 'three', '3000~4000')
+    into Salary values('Vuno', 3682, 'three', '3000~4000')
+    into Salary values('ADT Caps', 3016, 'three', '3000~4000')
+    into Salary values('Ahn Lab', 3000, 'three', '3000~4000')
+    into Salary values('Secui', 3941, 'three', '3000~4000')
+    into Salary values('Naver', 4500, 'four', '4000~5000')
+    into Salary values('KaKao', 4200, 'four', '4000~5000')
+    into Salary values('Google Korea', 4055, 'four', '4000~5000')
+select * from dual;
+commit;

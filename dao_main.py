ec281c1c6d547d10811f38e0c94a07ced69801d7
류선영 <84279479+tjsdud594@users.noreply.github.com>
2021-06-23 11:30:29 +0900

    dao_main 수정

--- dao_main.py
+++ dao_main.py
@@ -15,8 +15,7 @@ class EmpDAO:
                 sal_grade in (:sal_grade) and scale in (:scale) and hirecarrer in (:hirecarrer)", \
                     industry=industry, techstack=techstack, sal_grade=sal_grade, scale=scale, hirecarrer=hirecarrer)
             row = cur.fetchone()
-            data = '{"cname":"' + str(row[1]) + '", "industry":' + str(row[2]) + '", "techstack":' + (row[3]) + \
-            '", "sal_grade":' + (row[4]) + '", "scale":' + (row[5]) + '", "hirecarrer":' + (row[6]) + '}'
+            data = '{"cname":"' + str(row[1]) + '}'
 
 
         except Exception as e:
